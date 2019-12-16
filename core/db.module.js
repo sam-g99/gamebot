51c9f1c3a6758d1f9d38c631bdc87795b885f47d
aboxofsox <idkwthfml@gmail.com>
2019-12-16 14:52:33 -0600

    .

--- core/db.module.js
+++ core/db.module.js
@@ -2,7 +2,9 @@
     Currently I'm using MongoDB. For a few reasons. While SQL may be a much better alternative, in general. MongoDB offers a better way to manipulate data in the form of a JSON. Very little parsing needs to be done to get the data from point A to point B. Though, MongoDB IS scalable, I plan on moving to SQl in the near future.
 */
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/gamebot' || process.env.DB);
+
+mongoose.connect(process.env.DB); //FIXME: "MongoNetworkError" probably due to a bad url or related to the url in some way.
+
 const db = mongoose.connection;
 
 db.once('open', () =>{
