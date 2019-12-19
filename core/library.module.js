1db5a93246525304324c9d631744551f7aab161e
aboxofsox <idkwthfml@gmail.com>
2019-12-19 00:59:49 -0600

    .

--- core/library.module.js
+++ core/library.module.js
@@ -29,7 +29,7 @@ module.exports = {
         const res = await api.search(query);
 
         for(let i = 0; i < library.length; i++) {
-            if(library[i].title.includes(query)) {
+            if(library[i].title.contains(query)) {
                 library.splice(i, 1);
             }
         }
