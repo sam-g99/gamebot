e875a6c430ca10a8721ddea6da66102109e4df3a
aboxofsox <idkwthfml@gmail.com>
2019-12-24 16:44:04 -0600

    .

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(express.static('user'));
 
-app.get('/', (req, res) =>{
+app.get('', (req, res) =>{
     res.sendFile(`${__dirname}/user/redirect.html`);
 });
 /*app.get('/user', async (req, res) =>{
